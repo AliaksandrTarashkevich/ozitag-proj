9f660b58ff0cad2b0fc79ef2242f2ea1e189295b
Alex Tarashkevich <tal@db.by>
2020-11-02 14:04:00 +0300

    Fixing

--- js/index.js
+++ js/index.js
@@ -5,46 +5,41 @@ const navList = document.querySelector('.ozitag__navlist');
 
 const iconDrop = document.querySelectorAll('.js-icon-drop');
 
-iconDrop.forEach( el => {
+iconDrop.forEach(el => {
 
-	el.addEventListener('click', () => {
-		openTab(el);
-    })	
+    el.addEventListener('click', (event) => {
+        openTab(el);
+    })
 
 });
 
-function openTab(el){
-	let wrap = el.parentElement;
-	let parentLi = wrap.parentElement;
-	let ulDrop = wrap.nextElementSibling;
- 
-	if(ulDrop.classList.contains('opened')){
-		parentLi.classList.remove('active');
-		wrap.classList.remove('up');
-		ulDrop.classList.remove('opened');
-	} else {
+function openTab(el) {
+    let wrap = el.parentElement;
+    let parentLi = wrap.parentElement;
+    let ulDrop = wrap.nextElementSibling;
 
-		closeAllTab(parentLi);
-		parentLi.classList.add('active');
-		wrap.classList.add('up');
-		ulDrop.classList.add('opened');
-	}
+    if (ulDrop.classList.contains('opened')) {
+        parentLi.classList.remove('active');
+        ulDrop.classList.remove('opened');
+    } else {
+        closeAllTab(parentLi);
+        parentLi.classList.add('active');
+        ulDrop.classList.add('opened');
+    }
 }
 
 function closeAllTab(el) {
 
-	const ulDroped = el.parentElement.querySelectorAll('.ul-drop.opened');
-	const activeLi = el.parentElement.querySelectorAll('.dropdown.active');
+    const ulDropped = el.parentElement.querySelectorAll('.ul-drop.opened');
+    const activeLi = el.parentElement.querySelectorAll('.dropdown.active');
 
-	activeLi.forEach(elem => {
-		elem.classList.remove('active');
-		elem.children[0].classList.remove('up');
-	})
+    activeLi.forEach(elem => {
+        elem.classList.remove('active');
+    })
 
-
-	ulDroped.forEach(elem => {
-		elem.classList.remove('opened');
-	})
+    ulDropped.forEach(elem => {
+        elem.classList.remove('opened');
+    });
 }
 
 function bgCheck() {
